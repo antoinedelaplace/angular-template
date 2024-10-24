7c016d3f3e4e03008a9ba50a92e0a3a426b768f1
antoinedelaplace <antoine.delaplace@gmail.com>
2024-10-24 11:09:42 +0200

    Add Notification service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewContainerRef } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { FooterComponent } from './core/layout/footer/footer.component';
 import { HeaderComponent } from './core/layout/header/header.component';
+import { NotificationService } from './core/notification/notification.service';
 
 @Component({
 	selector: 'app-root',
@@ -11,4 +12,13 @@ import { HeaderComponent } from './core/layout/header/header.component';
 	imports: [CommonModule, RouterModule, FooterComponent, HeaderComponent],
 	templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit {
+	constructor(
+		private notificationService: NotificationService,
+		private viewContainerRef: ViewContainerRef
+	) {}
+
+	public ngAfterViewInit(): void {
+		this.notificationService.setContainerRef(this.viewContainerRef);
+	}
+}
